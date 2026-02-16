ea3a7a450588d38828bdd5bf7f80543a6f15e7d2
rattaseppkevin-tech <rattaseppkevin@gmail.com>
2026-02-16 17:36:45 +0200

    1

--- Numpy Primer/12_Where_Functions.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-my_array = np.arange(20) # Create a NumPy array with values from 0 to 19
-
-print(np.where(my_array % 2 == 0, "even", "odd")) # Use np.where to create an array of "even" and "odd" based on the values in my_array
-
-print(np.where(my_array % 2 == 0, "even", np.where(my_array == 9, my_array, "odd"))) # Use nested np.where to create an array where even numbers are labeled "even", the number 9 is kept as is, and odd numbers are labeled "odd"
