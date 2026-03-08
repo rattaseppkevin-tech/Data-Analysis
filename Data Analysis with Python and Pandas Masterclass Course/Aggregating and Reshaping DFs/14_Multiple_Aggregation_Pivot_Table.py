2f8ef03f7daf61019af1d12b0cfdbf3c8849dff3
rattaseppkevin-tech <rattaseppkevin@gmail.com>
2026-03-08 11:16:13 +0200

    Pivot tables

---
+++ Data Analysis with Python and Pandas Masterclass Course/Aggregating and Reshaping DFs/14_Multiple_Aggregation_Pivot_Table.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+sales = pd.read_csv(r"C:\Users\Kevin\Desktop\transactions.csv")
+
+sales = pd.read_csv(r"C:\Users\Kevin\Desktop\transactions.csv")
+sales["date"] = pd.to_datetime(sales["date"])
+sales["month"] = sales["date"].dt.month
+
+sales = sales.assign(
+    target_pct = sales["transactions"] / 2500,
+    met_target = (sales["transactions"] / 2500) >= 1,
+    bonus_payable = ((sales["transactions"] / 2500) >= 1) * 100,
+    month = sales["date"].dt.month,
+    day_of_week = sales["date"].dt.dayofweek)
+
+
+conversion = sales.pivot_table(index = "store_nbr",
+                         columns = "date",
+                         values = "transactions",
+                         aggfunc = ({"transactions": ["sum", "mean"]}))
+
+print(conversion)
